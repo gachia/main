1cffa8984ef165ceeb47ee28c130df97f1a7258b
gachia <massreduction@outlook.com>
2019-09-29 09:52:02 +0800

    Fixed CheckStyle
    Fixed CheckStyle errors and passed all JUnit Tests

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -14,7 +14,6 @@ import seedu.address.model.person.Remark;
  * Parses input arguments and creates a new {@code RemarkCommand} object
  */
 public class RemarkCommandParser implements Parser<RemarkCommand> {
-
     /**
      * Parses the given {@code String} of arguments in the context of the {@code RemarkCommand}
      * and returns a {@code RemarkCommand} object for execution.
